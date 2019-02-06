57ff071469b2a391f4b26cc7a5016b144d361d62
silencial <shawnxiex@gmail.com>
2019-02-06 15:37:28 -0800

    multi calibration dots detection

--- dot_detection2.py
+++ dot_detection2.py
@@ -5,39 +5,42 @@ import cv2 as cv
 cap = cv.VideoCapture(0)
 
 while(1):
-    # get a frame
-    ret, img = cap.read()
-    # opencv index use height*width
-    height, width, channel = img.shape
+	# get a frame
+	ret, img = cap.read()
+	fps = cap.get(5)
+	print(fps)
+	# opencv index use height*width
+	height, width, channel = img.shape
 
-    low_range = np.array([150, 100, 220])
-    high_range = np.array([250, 200, 255])
+	low_range = np.array([150, 100, 220])
+	high_range = np.array([250, 200, 255])
 
-    # start = time.time()
+	# start = time.time()
 
-    mask = cv.inRange(img, low_range, high_range)
-    points = cv.findNonZero(mask)
-    if (points is None):
-    	center = ((0, 0))
-    else:	
-	    # average these points
-	    avg = np.mean(points, axis=0)
-	    avg = avg[0]
-	    coord = (avg[0]/height, avg[1]/width)
+	mask = cv.inRange(img, low_range, high_range)
+	points = cv.findNonZero(mask)
+	if (points is None):
+		center = (0, 0)
+	else:	
+		# average these points
+		avg = np.mean(points, axis=0)
+		avg = avg[0]
+		coord = (avg[0]/height, avg[1]/width)
 
-	    center = (int(avg[0]), int(avg[1]))
+		center = (int(avg[0]), int(avg[1]))
+		
 
-    # end = time.time()
-    # print(end - start)
-    # print(count)
-    cv.circle(img, center, 5, (0,255,0), -1)
+	# end = time.time()
+	# print(end - start)
+	# print(count)
+	cv.circle(img, center, 5, (0,255,0), -1)
 
-    # show a frame
-    cv.imshow("capture", img)
-    cv.imshow("mask", mask)
-    # quit if 'q' is pressed
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
+	# show a frame
+	cv.imshow("capture", img)
+	cv.imshow("mask", mask)
+	# quit if 'q' is pressed
+	if cv.waitKey(1) & 0xFF == ord('q'):
+		break
 cap.release()
 cv.destroyAllWindows()
 
