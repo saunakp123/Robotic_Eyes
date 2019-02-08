1bca083d713e9c160e74ab1f2a6f6d8e4291c784
silencial <shawnxiex@gmail.com>
2019-02-08 11:09:26 -0800

    video saver

--- multidots_detection.py
+++ multidots_detection.py
@@ -1,11 +1,11 @@
 import numpy as np
 import cv2 as cv
  
-img = cv.imread("test_pictures/multidots.jpg")
+img = cv.imread("test_pictures/multidots_black.jpg")
 gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
  
 circles1 = cv.HoughCircles(gray,cv.HOUGH_GRADIENT,1,
-100,param1=100,param2=30,minRadius=10,maxRadius=100)
+100,param1=100,param2=10,minRadius=10,maxRadius=100)
 
 circles = circles1[0,:,:]
 circles = np.uint16(np.around(circles))
@@ -17,5 +17,7 @@ for i in circles:
 
 cv.namedWindow('test', cv.WINDOW_NORMAL)
 cv.imshow('test',img)
+# cv.imwrite('test_results/multi_calibration_dots.jpg',img)
+
 cv.waitKey(0)
 cv.destroyAllWindows()
